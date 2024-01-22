0482849ba9f643fe431b4eb6f543eda339d21bd3
Arina <arinalun14@gmail.com>
2024-01-23 01:12:12 +0300

    fix(user-service): update NotEnoughMoneyException

--- user-service/src/main/java/ru/itmo/userservice/exceptions/NotEnoughMoneyException.java
+++ user-service/src/main/java/ru/itmo/userservice/exceptions/NotEnoughMoneyException.java
@@ -1,7 +1,6 @@
 package ru.itmo.userservice.exceptions;
 
 public class NotEnoughMoneyException extends Exception{
-
     public NotEnoughMoneyException() {
         super("Недостаточно средств на счету");
     }
