88a0cde39a1d4803d2e9c0a933a08a2ef470cde9
Arina <arinalun14@gmail.com>
2024-02-27 19:44:28 +0300

    fix(swagger): add swagger for AuthController

--- user-service/src/main/java/ru/itmo/userservice/controllers/AuthController.java
+++ user-service/src/main/java/ru/itmo/userservice/controllers/AuthController.java
@@ -1,14 +1,14 @@
 package ru.itmo.userservice.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.itmo.userservice.exceptions.UserAlreadyExistsException;
 import ru.itmo.userservice.model.dto.JwtRequestDto;
 import ru.itmo.userservice.model.dto.UserRegisterRequestDto;
@@ -20,6 +20,14 @@ import ru.itmo.userservice.services.AuthService;
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(
+            description = "Log in",
+            summary = "Log in to the system"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "401", description = "Incorrect username or password")
+    })
     @PostMapping("/login")
     public ResponseEntity<?> createAuthToken(@RequestBody @Valid JwtRequestDto request) {
         try {
@@ -29,6 +37,14 @@ public class AuthController {
         }
     }
 
+    @Operation(
+            description = "Register",
+            summary = "New user register"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User has successfully registered"),
+            @ApiResponse(responseCode = "400", description = "User with this username already exists")
+    })
     @PostMapping("/register")
     public ResponseEntity<?> register (@RequestBody @Valid UserRegisterRequestDto userRegisterRequest)  {
         try {
